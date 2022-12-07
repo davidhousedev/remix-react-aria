43ed592c4fc43376ccc436036f2059103549d54b
David House <david.house@butcherbox.com>
2022-12-07 13:16:32 -0500

    add react-aria and FocusScope example

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,20 @@
+import React from "react";
+import { FocusScope } from "react-aria";
+
 export default function Index() {
+  let [isOpen, setOpen] = React.useState(false);
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <button onClick={() => setOpen(true)}>Open</button>
+      {isOpen && (
+        <FocusScope contain restoreFocus autoFocus>
+          <label htmlFor="first-input">First Input</label>
+          <input id="first-input" />
+          <label htmlFor="second-input">Second Input</label>
+          <input id="second-input" />
+          <button onClick={() => setOpen(false)}>Close</button>
+        </FocusScope>
+      )}
+    </>
   );
 }
